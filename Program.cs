ab904354a6b6578e0d2f3949f9cc15d6b126b251
Radhika-222 <wandareradhika@gmail.com>
2023-11-13 16:41:22 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose employee type to calculate the Salary: \n 1. HR\n 2. Admin\n 3. Software Developer");
+            int employeeType = Convert.ToInt32(Console.ReadLine());
+            Employee employee = new Employee();
+            switch (employeeType)
+            {
+                case 1:
+                    employee.HR();
+                    break;
+                case 2:
+                    employee.Admin();
+                    break;
+                case 3:
+                    employee.SoftwareDeveloper();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid employee type!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
